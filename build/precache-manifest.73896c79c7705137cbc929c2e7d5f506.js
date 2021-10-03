aab66c2296e71cf3b7a4713dad2cb6a4b62986c6
Mishka <shplishka@gmail.com>
2021-10-03 20:59:36 +0300

    dd

---
+++ build/precache-manifest.73896c79c7705137cbc929c2e7d5f506.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1359c1ea02f4a2b65ea062b12726426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1846383cd8cbef352e72",
+    "url": "/static/css/main.daad9feb.chunk.css"
+  },
+  {
+    "revision": "1f9ba4207ba2b31c89ab",
+    "url": "/static/js/2.63761f4e.chunk.js"
+  },
+  {
+    "revision": "1288b966e23790c91a32ff7b8bd43b74",
+    "url": "/static/js/2.63761f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1846383cd8cbef352e72",
+    "url": "/static/js/main.8ce4c574.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  },
+  {
+    "revision": "15a8a959f7a1010f1e1d18aa46253e71",
+    "url": "/static/media/bw.15a8a959.jpg"
+  },
+  {
+    "revision": "7b3fbf579a0617e831e0ad9c4e6b7e91",
+    "url": "/static/media/download.7b3fbf57.png"
+  },
+  {
+    "revision": "986701a7e9d90fa73a96816dd6e59e0b",
+    "url": "/static/media/green.986701a7.jpg"
+  },
+  {
+    "revision": "08485faae8047e79df5cd9a2e60e74b2",
+    "url": "/static/media/red.08485faa.jpg"
+  },
+  {
+    "revision": "8719408852cac6d61874101e1bcdbabc",
+    "url": "/static/media/yew.87194088.jpg"
+  }
+]);
--- src/firebase_init.js
+++ src/firebase_init.js
@@ -1,6 +1,6 @@
-import Firebase from "firebase";
+const firebase = require('firebase/app');
 
-const FireaseConfig = Firebase.initializeApp({
+const FireaseConfig = firebase.initializeApp({
   apiKey: "AIzaSyA74MlsGp-nxeuyAGMhnScGniOhI_5t8CE",
   authDomain: "benpo-5f6ce.firebaseapp.com",
   databaseURL: "https://benpo-5f6ce-default-rtdb.firebaseio.com/",
